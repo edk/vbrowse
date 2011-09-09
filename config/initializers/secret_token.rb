b86c5786978170452772eddb6f3d592cca569f32
Eddy Kim <eddyhkim@gmail.com>
2011-09-08 20:40:20 -0700

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vbrowse::Application.config.secret_token = 'b188d395db8fecd0539188a34280fa6e9e2a664fe0e814ec7337f5fb3b67d96bb8d33f5a1780743320a259ec3ec21d507d98076754cabbe51ca8ca7c14f85f19'
